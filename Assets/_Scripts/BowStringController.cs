bea43767ab846dea2c6fc9151994c0e5eeb64b7b
theTscheZ <bandion88@gmail.com>
2024-03-26 17:56:43 +0100

    bow can shoot again

--- Assets/_Scripts/BowStringController.cs
+++ Assets/_Scripts/BowStringController.cs
@@ -2,19 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class BowStringController : MonoBehaviour
 {
     [SerializeField] private BowString bowStringRenderer;
-
     private XRGrabInteractable interactable;
-
     [SerializeField] private Transform midPointGrabObject, midPointVisualObject, midPointParent;
-
     [SerializeField] float bowStringStretchLimit = 0.69f;
-    
     private Transform interactor;
+    private float strength;
+    public UnityEvent OnBowStringPulled;
+    public UnityEvent<float> OnBowStringReleased;
     
     private void Awake()
     {
@@ -29,6 +29,9 @@ public class BowStringController : MonoBehaviour
     
     private void ResetBowString(SelectExitEventArgs args)
     {
+        OnBowStringReleased?.Invoke(strength);
+        strength = 0;
+        
         interactor = null;
         midPointGrabObject.localPosition = Vector3.zero;
         midPointVisualObject.localPosition = Vector3.zero;
@@ -38,6 +41,7 @@ public class BowStringController : MonoBehaviour
     private void PrepareBowString(SelectEnterEventArgs args)
     {
         interactor = args.interactorObject.transform;
+        OnBowStringPulled?.Invoke();
     }
     
     private void Update()
@@ -51,9 +55,7 @@ public class BowStringController : MonoBehaviour
             float midPointLocalZAbs = Math.Abs(midPointLocalSpace.z);
 
             HandleStringPushedBackToStart(midPointLocalSpace);
-
             HandleStringPulledBackToLimit(midPointLocalZAbs, midPointLocalSpace);
-            
             HandlePullingString(midPointLocalZAbs, midPointLocalSpace);
             
             bowStringRenderer.CreateString(midPointVisualObject.position);
@@ -65,14 +67,21 @@ public class BowStringController : MonoBehaviour
         // what happens when the string is pulled between point 0 and the limit
         if (midPointLocalSpace.z < 0 && midPointLocalZAbs < bowStringStretchLimit)
         {
+            strength = Remap(midPointLocalZAbs, 0, bowStringStretchLimit, 0, 1);
             midPointVisualObject.localPosition = new Vector3(0,0, midPointLocalSpace.z);
         }
+    }
+
+    private float Remap(float value, int fromMin, float fromMax, int toMin, int toMax)
+    {
+        return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
     }
 
     private void HandleStringPulledBackToLimit(float midPointLocalZAbs, Vector3 midPointLocalSpace)
     {
         if (midPointLocalSpace.z < 0 && midPointLocalZAbs >= bowStringStretchLimit)
         {
+            strength = 1;
             midPointVisualObject.localPosition = new Vector3(0,0, -bowStringStretchLimit);
         }
     }
@@ -81,6 +90,7 @@ public class BowStringController : MonoBehaviour
     {
         if(midPointLocalSpace.z >= 0)
         {
+            strength = 0;
             midPointVisualObject.localPosition = Vector3.zero;
         }
     }
